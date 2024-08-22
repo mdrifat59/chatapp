10482c051b3b95449443d2aa8fd53dd511d63f58
mdrifat59 <mdrifatulislam59@gmail.com>
2024-08-22 23:36:55 +0600

    Home page ar Navbar create with svg img creat or set

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import LoggedInUserRoute from '../PrivateRoute/LoggedInUser'
+import Navbar from '../Components/Navbar/Index'
 
 const Home = () => {
   return (
     <>
-      <div className='flex justify-center items-center h-screen'>
-        <div className='w-2/4 p-4 bg-teal-500'>adfllkajdf</div>
+      <div className='w-full h-screen relative'>
+        <div className='h-[250px] w-full bg-[#4A81D3]'>
+          <div className='w-3/5 bg-[#232323] rounded-md shadow-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'> <Navbar/></div>
+        </div>
       </div>
     </>
   )

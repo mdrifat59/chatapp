69482703da8c13154a31bbc474d078e443a802f4
mdrifat59 <mdrifatulislam59@gmail.com>
2024-09-04 12:15:05 +0600

    Friend Request button is  done and dynamicaly work

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,64 +1,95 @@
 import React, { useEffect, useState } from 'react'
 import { AddFriendIcon } from '../svg/AddFriend'
-import { getDatabase, ref, onValue } from "firebase/database";
-import { useSelector } from 'react-redux'; 
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import { useSelector } from 'react-redux';
 import { getDownloadURL, getStorage, ref as Ref } from 'firebase/storage';
 import avaterImg from "../assets/defaultimg/profile.png"
 
 const UserList = () => {
-  const db = getDatabase(); 
-  let usered = useSelector((state)=>state.login.loggedIn)
+  const db = getDatabase();
+  let usered = useSelector((state) => state.login.loggedIn)
   const storage = getStorage();
-  let [users, setUsers]= useState([])
+  let [users, setUsers] = useState([])
+  let [friendrequestlist, setFriendrequestlist] = useState([])
 
   useEffect(() => {
-  const userListRef = ref(db, 'users/' );
+    const userListRef = ref(db, 'users/');
     onValue(userListRef, (snapshot) => {
       let user = []
-      snapshot.forEach((userList)=>{
-          if(usered.uid != userList.key){
-            getDownloadURL(Ref(storage, userList.key)).then((downloadURL) => {
-               user.push({
-                ...userList.val(),
-                id: userList.key,
-                photoURL: downloadURL
-               })
-
-            }).catch((error)=>{
-              user.push({
-                ...userList.val(),
-                id: userList.key,
-                photoURL: null
-               }) 
-
-            }).then(()=>{
-                setUsers([...user])
+      snapshot.forEach((userList) => {
+        if (usered.uid != userList.key) {
+          getDownloadURL(Ref(storage, userList.key)).then((downloadURL) => {
+            user.push({
+              ...userList.val(),
+              id: userList.key,
+              photoURL: downloadURL
             })
-          }
-      }) 
+
+          }).catch((error) => {
+            user.push({
+              ...userList.val(),
+              id: userList.key,
+              photoURL: null
+            })
+
+          }).then(() => {
+            setUsers([...user])
+          })
+        }
+      })
     })
-  }, [db, usered.uid, storage]) 
-  
+  }, [db, usered.uid, storage]);
+
+  let handleRequst = (data) => {
+    set(push(ref(db, "friendrequest/")), {
+      sendername: usered.displayName,
+      senderid: usered.uid,
+      senderphoto: usered.photoURL ?? "/src/assets/profile.png",
+      receivername: data.username,
+      receiverid: data.id,
+      receiverphoto: data.photoURL ?? "/src/assets/profile.png",
+    })
+  }
+
+  // show friendRequst
+  useEffect(() => {
+    const friendRequestRef = ref(db, 'friendrequest/');
+    onValue(friendRequestRef, (snapshot) => {
+      let reqarr = []
+      snapshot.forEach((item) => {
+        reqarr.push(item.val().senderid + item.val().receiverid)
+
+      })
+      setFriendrequestlist(reqarr)
+    });
+  }, [db])
+
   return (
     <div className='p-4'>
       <h1 className='font-robotoRegular text-2xl m-y-5'>All Users</h1>
-     {
-      users.map((item)=>(
-        <div className='flex items-center justify-between mt-5'>
-        <div className='flex items-center gap-x-2'>
-          <div className='w-12 h-12 rounded-full bg-blue-300 overflow-hidden'>
-            <img src={item.photoURL || avaterImg} alt="" className='w-full h-full rounded-full overflow-hidden' />
-          </div>
-          <div className='font-robotoBold'>{item.username}</div>
-        </div>
-        <div>
-          <button><AddFriendIcon /></button>
-        </div>
-      </div>
-      ))
-     }
 
-      
+      {
+        users.map((item) => (
+          <div key={item.id} className='flex items-center justify-between mt-5'>
+            <div className='flex items-center gap-x-2'>
+              <div className='w-12 h-12 rounded-full bg-blue-300 overflow-hidden'>
+                <img src={item.photoURL || avaterImg} alt="" className='w-full h-full rounded-full overflow-hidden' />
+              </div>
+              <div className='font-robotoBold'>{item.username}</div>
+            </div>
+            {
+              friendrequestlist.includes(item.id + usered.uid) || friendrequestlist.includes(usered.uid + item.id) ?
+                <button className='bg-red-400 py-1 px-3 rounded-md' >Cencel</button>
+                :
+                <div onClick={() => handleRequst(item)}>
+                  <button><AddFriendIcon /></button>
+                </div>
+            }
+          </div>
+        ))
+      }
+
+
     </div>
   )
 }

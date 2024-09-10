5bc32b072ec5ca5a2aedfc17320b74ef37ec3070
mdrifat59 <mdrifatulislam59@gmail.com>
2024-09-10 10:34:58 +0600

    Message box message dynamicaly show with databash

--- src/Components/Messagebox/index.jsx
+++ src/Components/Messagebox/index.jsx
@@ -1,63 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { EmojiIcon } from '../../svg/Emoji'
 import { ImageIcon } from '../../svg/Image'
 import msgimg from '../../assets/trail.jpg'
 import msgimg2 from '../../assets/trail2.jpg'
 import { useSelector } from 'react-redux'
+import { getDatabase, onValue, push, ref, set } from 'firebase/database'
+import { formatDistance } from 'date-fns'
 
 const MessageBox = () => {
-    let singlefriend = useSelector((single)=>single.active.active)
-  return (
-    <div className='w-full bg-white'>
-        <div className=' bg-[#232323] py-2 rounded-md mx-5 mt-3 '> 
-            <div className='ml-5 flex items-center gap-5'>
-                <div className='w-11 h-11 bg-orange-300 rounded-full'>
-                    <img src={singlefriend.profile} className='w-full h-full rounded-full overflow-hidden' alt="" />
+    let db = getDatabase()
+    let singlefriend = useSelector((single) => single.active.active)
+    let [message, setMessage] = useState('')
+    let [allmessage, setAllmessage] = useState([])
+    let usered = useSelector((state) => state.login.loggedIn)
+
+    let handleSendMessage = () => {
+        if (singlefriend?.status == "single") {
+            set(push(ref(db, 'singlemessage')), {
+                whosendname: usered.displayName,
+                whosendid: usered.uid,
+                whoreceivename: singlefriend.name,
+                whoreceiveid: singlefriend.id,
+                message: message,
+                date: `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}-${new Date().getHours()}:${new Date().getMinutes()}`
+            })
+        }
+        setMessage('')
+    }
+
+    //  get message
+    useEffect(() => {
+        onValue(ref(db, "singlemessage/"), (snapshot) => {
+            let messagearr = []
+            snapshot.forEach((item) => {
+                if (usered.uid == item.val().whosendid && item.val().whoreceiveid == singlefriend.id || usered.uid == item.val().whoreceiveid && item.val().whosendid == singlefriend.id) {
+                    messagearr.push(item.val())
+                }
+            })
+            setAllmessage(messagearr)
+        })
+    }, [singlefriend.id])
+
+    return (
+        <div className='w-full bg-white'>
+            <div className=' bg-[#232323] py-2 rounded-md mx-5 mt-3 '>
+                <div className='ml-5 flex items-center gap-5'>
+                    <div className='w-11 h-11 bg-orange-300 rounded-full'>
+                        <img src={singlefriend.profile} className='w-full h-full rounded-full overflow-hidden' alt="" />
+                    </div>
+                    <div className='text-white'>
+                        <span className='font-robotoRegular'>{singlefriend.name || "choose your friend"}</span>
+                    </div>
                 </div>
-                <div className='text-white'>
-                    <span className='font-robotoRegular'>{singlefriend.name || "choose your friend"}</span>
-                </div>
             </div>
-        </div>
-        <div className='h-[300px] bg-[#FFFFFF]  mx-5 overflow-x-auto'>
-            {/* sender site */}
-           <div className='w-[60%] ml-auto'>
-            <div className='bg-blue-500 font-robotoRegular inline-block rounded-md mt-2 py-2 px-4 text-white'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias illum magnam ratione, provident ipsa ab beatae eaque molestiae sequi necessitatibus. Repellendus saepe sit laboriosam? Rem sed ipsum odit. Molestiae, commodi.  
-            </div>
-           </div>
-            {/* receicer site */}
-           <div className='w-[60%] mr-auto'>
-            <div className='bg-stone-500 font-robotoRegular inline-block rounded-md mt-2 py-2 px-4 text-white'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores possimus fuga ex. Eveniet facilis quae perspiciatis iure dolorem magnam adipisci, doloremque iusto alias sit asperiores modi quia, minima molestias veritatis dolore culpa itaque. Praesentium facere, quaerat nam earum reprehenderit cupiditate velit magni laboriosam. Nesciunt consequuntur, nihil natus, non voluptates, deserunt reiciendis molestiae eius culpa adipisci vitae sit blanditiis deleniti omnis. Quam architecto nesciunt eius obcaecati? Error quidem praesentium eveniet unde similique atque natus blanditiis quisquam delectus omnis laudantium voluptates, sequi officia dolorum nihil ut cum ducimus nam cumque voluptatem aliquam iusto? At, consectetur animi cupiditate hic asperiores rerum repellendus facere totam eum perspiciatis error. Laudantium facilis, suscipit accusamus recusandae quaerat hic tenetur reiciendis similique adipisci enim ducimus voluptates corrupti veritatis neque animi ipsa ipsam, impedit minima repellat vitae quasi alias perspiciatis? Aliquam vel dolore quibusdam molestias. Minus, nesciunt commodi ad culpa sed vitae excepturi esse reiciendis, veritatis ullam earum quibusdam nostrum adipisci doloremque, libero porro! Quos molestias officia asperiores esse sunt harum, excepturi iusto, maxime numquam tempore a. Provident, iste ea alias, inventore dolorum ipsa a nemo consequatur, omnis rem qui. In aliquid nisi rem illum, repellendus officia, expedita sunt explicabo quidem fugit porro labore. Veritatis magnam quam assumenda dicta natus ducimus, voluptate id quia eum ipsa neque illum perspiciatis amet eaque, aut sequi culpa tempore temporibus iste quod iure laudantium nesciunt! Necessitatibus, placeat recusandae ratione aut suscipit et. Quisquam ullam deleniti soluta voluptatem eveniet minima quasi exercitationem doloribus earum vel est accusantium, cupiditate quod impedit iusto! Totam ratione, cupiditate quia rerum sunt, et sapiente blanditiis dicta quae deserunt officia beatae dolor quis aperiam corrupti. Mollitia ut officiis harum natus eos doloribus tenetur consectetur quod veritatis expedita quia dolorem nemo corrupti illo molestias, in odit quo! Facilis beatae rerum ipsa libero, voluptatum dolor perferendis laborum vero, enim ducimus quaerat iusto ex? Voluptate quisquam quidem voluptas illum voluptatibus, at molestias quos, quo minus quae numquam repellendus cupiditate unde maxime qui est eaque, dolorum eveniet distinctio dignissimos? Fugiat nesciunt ipsum perspiciatis dolores sint quibusdam saepe, optio doloremque itaque dolor eveniet est reprehenderit sapiente voluptas molestiae cum minima? Ullam et voluptas, ducimus error neque quidem tempora atque perferendis aliquam ratione deleniti, obcaecati eius excepturi ut doloribus nulla! Expedita similique repellendus laboriosam cupiditate magnam, harum, voluptatem vel quod excepturi reprehenderit maxime. Commodi cupiditate aliquam eum aut, doloremque sunt culpa tempora ipsa exercitationem suscipit modi veniam quasi in nesciunt magni facilis maxime placeat, consequatur quas. Inventore tempora ipsum sapiente, dicta voluptatibus blanditiis dolore amet voluptate omnis perspiciatis quibusdam et! In eaque reprehenderit provident delectus quasi. Impedit minima consequuntur rerum eligendi voluptatum iure dicta. Dolor repudiandae inventore neque omnis blanditiis vero illum, saepe enim. Assumenda corporis quam illum, rerum eligendi repudiandae veritatis quasi est eius minus consectetur non vitae earum nihil! Asperiores sequi quam, natus veritatis facilis similique esse! Natus, ducimus! Ipsam totam aliquam labore odit unde rerum? Officiis culpa dolore consequatur exercitationem ex minima voluptatum deserunt harum blanditiis quasi neque magni veritatis, ratione molestias. Velit, officia esse. Veniam aspernatur quisquam nam officia, ipsa quidem maiores!
-            </div>
-           </div>
-            {/* sender site img */}
-           <div className='w-[60%] ml-auto overflow-hidden'>
+            <div className='h-[300px] bg-[#FFFFFF]  mx-5 overflow-x-auto'>
+                {
+                    singlefriend?.status == "single" ?
+                        allmessage.map((item, i) => (
+                            <div key={item.i}>
+                                {
+                                    item.whosendid == usered.uid ? (
+                                        <div className='w-[60%] ml-auto flex flex-col items-end'>
+                                            <p className='bg-blue-500 font-robotoRegular inline-block rounded-md mt-2 py-2 px-4 text-white'>
+                                                {item.message}
+                                            </p>
+                                            <span className='mt-2 text-sm'>{formatDistance(item.date, new Date(), { addSuffix: true })}</span>
+                                        </div>
+                                    ) : (
+                                        <div className='w-[60%] mr-auto flex flex-col items-start'>
+                                            <p className='bg-stone-500 font-robotoRegular inline-block rounded-md mt-2 py-2 px-4 text-white'>
+                                                {item.message}
+                                            </p>
+                                            <span className='mt-2 text-sm'>{formatDistance(item.date, new Date(), { addSuffix: true })}</span>
+                                        </div>
+                                    )}
+                            </div>
+                        ))
+                        :
+
+                        " "
+                }
+
+
+                {/* sender site img */}
+                {/* <div className='w-[60%] ml-auto overflow-hidden'>
             <div className='bg-blue-500 font-robotoRegular inline-block rounded-md mt-2 py-2 px-4 '>
                 <img src={msgimg2}  className='w-full h-full object-cover rounded-md ' alt="" />
             </div>
-           </div>
-            {/* receicer site img */}
-           <div className='w-[60%] mr-auto overflow-hidden'>
+           </div> */}
+                {/* receicer site img */}
+                {/* <div className='w-[60%] mr-auto overflow-hidden'>
             <div className='bg-stone-300 font-robotoRegular inline-block rounded-md mt-2 py-2 px-4 '>
                 <img src={msgimg}  className='w-full h-full object-cover rounded-md' alt="" />
             </div>
-           </div>
-           
-        </div>
-        <div className='bg-[#F5F5F5] mx-5 py-3 shadow-lg rounded-md flex items-center justify-center'> 
-        <div className='w-[450px] bg-[#FFFFFF]  flex justify-center rounded-md gap-2'>
-            <div className='flex justify-center items-center gap-2'>
-                <EmojiIcon/>
-                <ImageIcon/>
+           </div> */}
+
             </div>
-                <input className='py-2 px-2 outline-none w-[60%]' type="text" placeholder='type anything' />
-                <button className=' font-robotoRegular px-7 bg-[#4A81D3] rounded-md text-white'>Send</button>
+            <div className='bg-[#F5F5F5] mx-5 py-3 shadow-lg rounded-md flex items-center justify-center'>
+                <div className='w-[450px] bg-[#FFFFFF]  flex justify-center rounded-md gap-2'>
+                    <div className='flex justify-center items-center gap-2'>
+                        <EmojiIcon />
+                        <ImageIcon />
+                    </div>
+                    <input className='py-2 px-2 outline-none w-[60%]' type="text" placeholder='type anything' onChange={(e) => setMessage(e.target.value)} value={message} />
+                    <button className=' font-robotoRegular px-7 bg-[#4A81D3] rounded-md text-white' onClick={handleSendMessage}>Send</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default MessageBox
